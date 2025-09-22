2e335a4c1d21ef08cd63177ce93c7cddbab316da
bre-vin <vinniebstone@gmail.com>
2025-09-22 17:31:14 -0400

    HTML in JS

--- src/index.js
+++ src/index.js
@@ -7,19 +7,29 @@ function search(event) {
   axios.get(apiUrl).then(updateTemperature);
 }
 
+function getForecastData(city) {
+  let apiKey = `dfc8abb9a745462f6o46f358det48310`;
+  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
+
 function updateTemperature(response) {
   let temperatureElement = document.querySelector("#current-temperature");
-  temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+  let temperature = response.data.temperature.current;
   let currentCity = document.querySelector("#current-city");
-  currentCity.innerHTML = response.data.city;
   let weatherDescription = document.querySelector("#weather-description");
-  weatherDescription.innerHTML = response.data.condition.description;
   let humidityElement = document.querySelector("#humidity");
-  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
   let windElement = document.querySelector("#wind");
-  windElement.innerHTML = `${response.data.wind.speed}km/h`;
   let weatherIconElement = document.querySelector("#weather-icon");
+
   weatherIconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-icon" />`;
+  temperatureElement.innerHTML = Math.round(temperature);
+  currentCity.innerHTML = response.data.city;
+  weatherDescription.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  windElement.innerHTML = `${response.data.wind.speed}km/h`;
+
+  getForecastData(response.data.city);
 }
 
 function formatDate(date) {
@@ -56,3 +66,24 @@ let currentDateELement = document.querySelector("#current-date");
 let currentDate = new Date();
 
 currentDateELement.innerHTML = formatDate(currentDate);
+
+function displayForecast() {
+  let days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
+  let forcastHTML = "";
+
+  days.forEach(function (day) {
+    forcastHTML =
+      forcastHTML +
+      `
+    <div class="weather-forecast-day">
+      <div class="weather-forecast-date">${day}</div>
+      <div class="weather-forecast-icon"></div>
+      <div class="weather-forecast-temperature">
+        <div class="weather-forecast-temperature-max"><strong>18°</strong></div>
+        <div class="weather-forecast-temperature-min">12°</div>
+      </div>
+    </div>`;
+  });
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = forcastHTML;
+}
